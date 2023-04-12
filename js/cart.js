cf7ade9f374f6bc964662def1c3ba7686e4e5c8d
Flexer678 <81823862+Flexer678@users.noreply.github.com>
2023-04-12 09:04:41 -0600

    email and search added

--- js/cart.js
+++ js/cart.js
@@ -1,29 +1,20 @@
+
+
 //import { object } from "firebase-functions/v1/storage";
 let display = document.querySelector(".cartitems")
 let cartbtn = document.querySelector(".cart")
 let cartnum = document.querySelector(".cartNum")//the number on the cart
-let cartIcon = document.querySelector(".cartIcon")
 
-let myCart = []
-let cart = sessionStorage.getItem("cart")
-sessionStorage.setItem('cart', JSON.stringify(myCart));
+
+
+sessionStorage.setItem('cart', JSON.stringify([]));
 //localStorage.setItem('cart', myCart);
 //cartnum.innerHTML = Object.keys(myCart).length
 
 
-cartIcon.addEventListener("click",()=>{
-    window.location.href = "./cart.html"
-   
 
-})
 
-window.addEventListener("load",(event)=>{
-    alert(window.location.href)
-    if (window.location.href == "cart.html"){
-    display.innerHTML = JSON.parse(sessionStorage.getItem("cart"))
-    }
-})
-
+console.log(typeof(JSON.parse(sessionStorage.getItem("cart"))))
 
 
 //bodyq.style.background = "red"
@@ -40,15 +31,18 @@ function BuyItem(item){
 
 function addCart(element){
     alert(element)
-    myCart.push(JSON.parse(element))
-    sessionStorage.setItem('cart', JSON.stringify(cart.push(element)));
-    console.log("mycart",myCart)
-    console.log("localstorgae", JSON.parse(sessionStorage.getItem("cart")) )
+    let data = JSON.stringify(element)
+    //data.push(JSON.parse(element))
+    //data = data;
+    sessionStorage.setItem('cart', JSON.stringify(sessionStorage.getItem("cart")),data);
+    //console.log("mycart",myCart)
+    console.log("parsed", JSON.stringify(sessionStorage.getItem("cart")))
     
     //cartnum.innerHTML = object.keys(localStorage.getItem("cart")).length
     
 }
 
 
+//https://formsubmit.co/el/kigizi
 
 export{addCart}
---
+++ js/cartIcon.js
@@ -0,0 +1,7 @@
+let cartIcon = document.querySelector(".cartIcon")
+
+cartIcon.addEventListener("click", () => {
+    window.location.href = "./cart.html"
+
+
+})
